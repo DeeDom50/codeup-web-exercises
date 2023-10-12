5c3c3553e1d87baddc0180d55eb253fd5358fb9e
raedownen <rae.l.downen@gmail.com>
2023-10-12 14:09:08 -0500

    update

--- js/iterating.js
+++ js/iterating.js
@@ -6,13 +6,14 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-let names = [Bill, Bob, Tom, Jerry]
+const names = ['Bill', 'Bob', 'Tom', 'Jerry']
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-    console.log (names.length);
+    const numOfNames = names.length;
+    console.log ("Number of names => ", numOfNames);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
@@ -27,14 +28,20 @@ let names = [Bill, Bob, Tom, Jerry]
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-    for(let i = 0; i < names.length; i++){
-        console.log(names[0]);
+    for (let i = 0; i < names.length; i++){
+        console.log(names[i]);
+        
+        for (let name of names) {
+        console.log(name);
+        }
+    }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-    names.forEach(function (name){
-        console.log(name)
+    names.forEach((name)=>{
+        console.log(name);
+    });
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -48,19 +55,20 @@ let names = [Bill, Bob, Tom, Jerry]
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-    function first(arr){
-        return arr[0];
-    }
-        console.log(first(names));
-
-        function second(arr){
-            return arr[1];
-        }
-        console.log(second(names));
-
-        function last(arr) {
-            return arr[arr.length-1];
-        }
-        console.log(last(names));
-
+    const first = (array) => {
+        let result = array[0];
+        return result;
+    };
+    const second = (array) => {
+        let result = array[1];
+        return result;
+    };
+    const last = (array) => {
+        let arrayLength = array.length;
+        let result = array[arrayLength - 1];
+        return result;
+    };
+    console.log(first(names));
+    console.log(second(names));
+    console.log(last(names));
 })();
